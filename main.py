813a39c0af97663ce5557e21eb15ae32847e51b8
Huỳnh Đào <huynhdao0808@gmail.com>
2020-03-16 08:32:37 +0700

    final

--- main.py
+++ main.py
@@ -4,86 +4,85 @@ import sqlite3
 
 app = Flask(__name__)
 
-conn = sqlite3.connect('tiki.db')
-cur = conn.cursor()
-
-def get_number_total():
+def get_number_total(cur):
     query = """
         SELECT MAX(products.id)
         FROM products
         """
     nototal = cur.execute(query)
-    return nototal
+    return nototal.fetchall()
 
-def get_number_categories():
+def get_number_categories(cur):
     query = """
         SELECT MAX(categories.id)
         FROM categories
         """
     nocat = cur.execute(query)
-    return nocat
+    return nocat.fetchall()
 
-def get_number_brand():
+def get_number_brand(cur):
     query = """
         SELECT COUNT(DISTINCT brand)
         FROM products
         """
     nobrand = cur.execute(query)
-    return nobrand
+    return nobrand.fetchall()
 
-def get_average_price():
+def get_average_price(cur):
     query = """
-        SELECT AVERAGE(price)
+        SELECT AVG(price)
         FROM products
         """
     avgprice = cur.execute(query)
-    return avgprice
+    return int(avgprice.fetchall()[0][0])
 
-def get_top5_brands():
+def get_top5_brands(cur):
     query = """
         SELECT brand, COUNT(name) as count
         FROM products
         GROUP BY brand
-        ORDER BY count DES
-        LIMIT 5
+        ORDER BY count DESC
+        LIMIT 5 OFFSET 5
         """
     top5brand = cur.execute(query)
-    return top5brand
+    return top5brand.fetchall()
 
-def get_top5_cats():
+def get_top5_cats(cur):
     query = """
         SELECT categories.name, COUNT(products.name) as count
         FROM products
-        INNER JOINT categories ON categories.id = products.cat_id
+        INNER JOIN categories ON categories.id = products.cat_id
         GROUP BY cat_id
-        ORDER BY count DES
+        ORDER BY count DESC
         LIMIT 5
         """
     top5cat = cur.execute(query)
-    return top5cat
+    return top5cat.fetchall()
 
-def get_top5_highest():
+def get_top5_highest(cur):
     query = """
         SELECT name, price , img_link, url
         FROM products
-        ORDER BY price DES
+        ORDER BY price DESC
         LIMIT 5
         """
     top5cat = cur.execute(query)
-    return top5cat
+    return top5cat.fetchall()
 
 @app.route('/')
 def index():
     data = {}
-    data['nototal'] = get_number_total
-    data['nocat'] = get_number_categories
-    data['nobrand'] = get_number_brand
-    data['avgprice'] = get_average_price
-    data['top5brands'] = get_top5_brands
-    data['top5cats'] = get_top5_cats
-    data['top5highest'] = get_top5_highest
+    conn = sqlite3.connect('tiki.db')
+    cur = conn.cursor()
+    data['nototal'] = get_number_total(cur)
+    data['nocat'] = get_number_categories(cur)
+    data['nobrand'] = get_number_brand(cur)
+    data['avgprice'] = get_average_price(cur)
+    data['top5brands'] = get_top5_brands(cur)
+    data['top5cats'] = get_top5_cats(cur)
+    data['top5highest'] = get_top5_highest(cur)
     return render_template('index.html',data=data)
 
 if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=8000, debug=True)
+  app.run(host='0.0.0.0', port=5000, debug=True)
  
